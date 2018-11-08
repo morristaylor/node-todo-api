7f040d728384458d15ad6381abaf8563517d7440
Taylor Morris <taylor.morris@gmail.com>
2018-11-08 11:25:11 -0800

    added model validators and set up basic new user example

--- server/server.js
+++ server/server.js
@@ -5,13 +5,18 @@ mongoose.connect('mongodb://localhost:27017/TodoApp');
 
 var Todo = mongoose.model('Todo', {
   text: {
-    type: String
+    type: String,
+    required: true,
+    minlength: 1,
+    trim: true
   },
   completed: {
-    type: Boolean
+    type: Boolean,
+    default: false
   },
   completedAt: {
-    type: Number
+    type: Number,
+    default: null
   }
 });
 
@@ -19,20 +24,27 @@ var newTodo = new Todo({
   text: 'Cook dinner'
 });
 
-// newTodo.save().then((doc) => {
-//   console.log('Saved todo', doc);
-// }, (e) => {
-//   console.log('Unable to save todo');
-// });
-
-var otherTodo = new Todo({
-  text: 'Walk the dog',
-  completed: true,
-  completedAt: 123
-});
-
-otherTodo.save().then((doc) => {
+newTodo.save().then((doc) => {
   console.log('Saved todo', doc);
 }, (e) => {
   console.log('Unable to save todo');
+});
+
+var User = mongoose.model('User', {
+  email: {
+    type: String,
+    required: true,
+    minlength: 1,
+    trim: true
+  }
+});
+
+var newUser = new User({
+  email: 'taylor@test.com'
+})
+
+newUser.save().then((doc) => {
+  console.log('User saved', doc);
+}, (e) => {
+  console.log('Unabe to save user');
 });
