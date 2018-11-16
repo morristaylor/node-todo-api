dcaa2ea7388ca910adc6a41291b4c2f39b1e7b7d
Taylor Morris <taylor.morris@gmail.com>
2018-11-16 08:49:47 -0800

    added PATCH /todos/:id route, added lodash for pick()

--- server/server.js
+++ server/server.js
@@ -1,6 +1,7 @@
-var express = require('express');
-var bodyParser = require('body-parser');
-var {ObjectID} = require('mongodb');
+const _ = require('lodash');
+const express = require('express');
+const bodyParser = require('body-parser');
+const {ObjectID} = require('mongodb');
 
 var {mongoose} = require('./db/mongoose.js');
 var {Todo} = require('./models/todo');
@@ -19,7 +20,7 @@ app.post('/todos', (req, res) => {
   todo.save().then((doc) => {
     res.send(doc);
   }, (e) => {
-    res.status(400).send(e);
+    res.sendStatus(400);
   });
 });
 
@@ -27,7 +28,7 @@ app.get('/todos', (req, res) => {
   Todo.find().then((todos) => {
     res.send({todos});
   }, (e) => {
-    res.status(400).send(e);
+    res.Sendstatus(400);
   });
 });
 
@@ -35,17 +36,17 @@ app.get('/todos/:id', (req, res) => {
   var id = req.params.id
 
   if (!ObjectID.isValid(id)) {
-    return res.sendStatus(404).send();
+    return res.sendStatus(404);
   }
 
   Todo.findById(id).then((todo) => {
     if (!todo) {
-      return res.sendStatus(404).send();
+      return res.sendStatus(404);
     }
 
     res.send({todo});
   }).catch((e) => {
-    res.sendStatus(400).send();
+    res.sendStatus(400);
   });
 });
 
@@ -53,18 +54,44 @@ app.delete('/todos/:id', (req, res) => {
   var id = req.params.id
 
   if (!ObjectID.isValid(id)) {
-    return res.sendStatus(404).send();
+    return res.sendStatus(404);
   }
 
   Todo.findOneAndDelete({_id: id}).then((todo) => {
     if (!todo) {
-      return res.sendStatus(404).send
+      return res.sendStatus(404)
     }
 
     res.send({todo});
   }).catch((e) => {
-    res.sendStatus(400).send();
+    res.sendStatus(400);
     });
+  });
+
+  app.patch('/todos/:id', (req, res) => {
+    var id = req.params.id
+    var body = _.pick(req.body, ['text', 'completed']);
+
+    if (!ObjectID.isValid(id)) {
+      return res.sendStatus(404);
+    }
+
+    if (_.isBoolean(body.completed) && body.completed) {
+      body.completedAt = new Date().getTime();
+    } else {
+      body.completed = false;
+      body.completedAt = null;
+    }
+
+    Todo.findOneAndUpdate({_id: id}, {$set: body}, {new: true}).then((todo) => {
+      if (!todo) {
+        return res.sendStatus(404);
+      }
+
+      res.send({todo});
+    }).catch((e) => {
+      res.sendStatus(400);
+    })
   });
 
 app.listen(port, () => {
